dcb79b15540269842c8893939122120045993a17
Christine Chynoweth <one171717@Christines-MacBook.local>
2018-06-25 23:38:55 -0400

    Update1

---
+++ config.py
@@ -0,0 +1,6 @@
+consumer_key = "dw42wJFtNT4qfcyZhQDlOZ1ma"
+consumer_secret = "mfhoqIPLR1rccjuPAw4dpI9tsujfy3pOgy3M7Nt2i1Sbtn4SJ3"
+access_token = "261841526-EhF8bRtP27b7rm95IPtOw6Gz9y3KsvFJtgmHJ7vI"
+access_token_secret = "MlivTgZSAj2RcFMGoOglsOQ2eJoRYsVV4vCnWRlCbBiP5"
+
+
